9090edda212aa6035a9357f9db934c84c33ad0ee
KingEvil55 <98324609+KingEvil55@users.noreply.github.com>
2023-12-03 09:58:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/KingEvil55/DAXXMUSIC",
+    "https://github.com/KingEvil55/YumiMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
